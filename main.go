60a9e92d295d48617f974763d989107d89421d28
Bálint Kemény <balintkemeny@gmail.com>
2022-02-27 20:39:38 +0100

    implement syllable parsing and basic vowel conversion

--- main.go
+++ main.go
@@ -2,31 +2,87 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
-var characterMap = map[rune][4]rune{
-	'a': {'ā', 'á', 'ǎ', 'à'},
-	'o': {'ō', 'ó', 'ǒ', 'ò'},
-	'e': {'ē', 'é', 'ě', 'è'},
-	'i': {'ī', 'í', 'ǐ', 'ì'},
-	'u': {'ū', 'ú', 'ǔ', 'ù'},
-	'ü': {'ǖ', 'ǘ', 'ǚ', 'ǜ'},
+var characterMap = map[rune][5]rune{
+	'a': {'a', 'ā', 'á', 'ǎ', 'à'},
+	'o': {'o', 'ō', 'ó', 'ǒ', 'ò'},
+	'e': {'e', 'ē', 'é', 'ě', 'è'},
+	'i': {'i', 'ī', 'í', 'ǐ', 'ì'},
+	'u': {'u', 'ū', 'ú', 'ǔ', 'ù'},
+	'ü': {'ü', 'ǖ', 'ǘ', 'ǚ', 'ǜ'},
 }
 
-func parseToSyllables(raw string) []string {
-	re := regexp.MustCompile("[1-4]+")
-	fmt.Println(re.FindAllString("ni3hao3", -1))
-	fmt.Println(re.FindAllStringIndex("ni3hao3", -1))
-	return []string{}
+var vowelOrder = map[rune]int{
+	'a': 5,
+	'o': 4,
+	'e': 3,
+	'i': 2,
+	'u': 1,
+	'ü': 0,
 }
 
-func convertSyllable(syllable string) string {
-	return syllable
+type syllable struct {
+	letters string
+	tone    int
+}
+
+func execute(raw string) string {
+	syllables := parseToSyllables(raw)
+	converted := ""
+	for _, s := range syllables {
+		converted += s.convert()
+	}
+
+	return converted
+}
+
+func parseToSyllables(raw string) []syllable {
+	trimmed := strings.Trim(raw, "\n\t")
+	re := regexp.MustCompile("[0-4]+")
+
+	tones := re.FindAllString(trimmed, -1)
+	strSlice := re.Split(trimmed, -1)
+
+	syllables := []syllable{}
+	for i, s := range strSlice {
+		if s == "" {
+			continue
+		}
+		tone, _ := strconv.Atoi(tones[i])
+		syllables = append(syllables, syllable{letters: s, tone: tone})
+	}
+	return syllables
+}
+
+func (s *syllable) convert() string {
+	if s.tone == 0 {
+		return s.letters
+	}
+
+	mainVowel, mainVowelIndex := determineMainVowel(s.letters)
+	return s.letters[:mainVowelIndex] + string(characterMap[mainVowel][s.tone]) + s.letters[mainVowelIndex+1:]
+}
+
+func determineMainVowel(letters string) (rune, int) {
+	var maxVowelValue, maxVowelIndex int
+	for i, r := range letters {
+		if _, ok := vowelOrder[r]; !ok {
+			continue
+		}
+		if vowelOrder[r] >= maxVowelValue {
+			maxVowelValue = vowelOrder[r]
+			maxVowelIndex = i
+		}
+	}
+
+	return rune(letters[maxVowelIndex]), maxVowelIndex
 }
 
 func main() {
-	_ = parseToSyllables("")
-	_ = convertSyllable("")
-	fmt.Println(characterMap)
+	fmt.Println(execute(os.Args[1]))
 }
